38497857f4f1306f456daa17345dcbc13a39b11e
Sven Bischoff <sven@medienlampe.de>
2025-02-08 15:35:29 +0100

    Implements base layout.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
 import eleventySass from "eleventy-sass";
 
 export default function(eleventyConfig) {
+  eleventyConfig.setChokidarConfig({
+		usePolling: true,
+		interval: 500,
+	});
+  
   eleventyConfig.addPassthroughCopy({"_src/assets/img": "assets/img"});
 
   eleventyConfig.addPlugin(eleventySass, {
