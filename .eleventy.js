16716fa6c35fe1a11b445a8a13530c450f99b982
Sven Bischoff <sven@medienlampe.de>
2025-02-08 14:00:03 +0100

    Bootstrapping.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,28 @@
-module.exports = function(eleventyConfig) {
+import eleventySass from "eleventy-sass";
+
+export default function(eleventyConfig) {
+  // Output directory: _site
+
+  // Copy `img/` to `_site/img`
+  // eleventyConfig.addPassthroughCopy("static/img");
+
+  // Copy `css/fonts/` to `_site/css/fonts`
+  // Keeps the same directory structure.
+  // eleventyConfig.addPassthroughCopy({"static/css": "css"});
+
+  // Copy any .jpg file to `_site`, via Glob pattern
+  // Keeps the same directory structure.
+  // eleventyConfig.addPassthroughCopy("**/*.jpg");
+  eleventyConfig.addPlugin(eleventySass, {
+    compileOptions: {
+      permalink: function(contents, inputPath) {
+        return (data) => {
+          return data.page.filePathStem.replace(/^\/scss\//, "/css/") + ".css";
+        };
+      }
+    }
+  });
+
   return {  
     dir: {
       input: "_src"
